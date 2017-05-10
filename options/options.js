97b4b19e17207705c5a8e8f27311dd395f4f1152
Andrew Parrish <m.andrewparrish@gmail.com>
2017-05-10 16:37:25 -0400

    Style updates and add to priorities list implementation

--- options/options.js
+++ options/options.js
@@ -1,12 +1,30 @@
 function addTeamsToSelect(select_id) {
     $.getJSON("../mappings/teams.json", function(teams) {
         $.each(teams, function(val, name) {
-            console.log(val + " " + name);
             $(select_id).append($("<option>", { "value": val, "text": name }));
         });
     });
 }
 
+function addPriorityToSelect(select_id) {
+    for(i = 1; i <= $('#priority-list').length; i++) {
+        $(select_id).append($("<option>", { value: i, text: i }));
+    }
+}
+
 $(document).ready(function() {
     addTeamsToSelect('#add-team-select');
+    addPriorityToSelect('#add-team-priority');
+
+    var saveTeam = function() {
+        chrome.storage.sync.get('priorities', function(list) {
+            var priorityList = list;
+            if (priorityList.toString() === {}.toString()) { priorityList = [] }
+            priorityList.push({
+            
+            });
+        });
+    };
+
+    $("#add-team-button").click(saveTeam);
 });
