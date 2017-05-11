6bed349f768b66c7efbb38227c9176d94abdfc88
Andrew Parrish <aparrish@jibe.com>
2017-05-11 11:26:57 -0400

    More work on saving priorities

--- options/options.js
+++ options/options.js
@@ -1,3 +1,14 @@
+/*
+    Priority Model:
+    {
+        val: str,
+        text: str,
+        priority: int,
+        type: 'player || team'
+    }
+ */
+
+
 function addTeamsToSelect(select_id) {
     $.getJSON("../mappings/teams.json", function(teams) {
         $.each(teams, function(val, name) {
@@ -12,17 +23,48 @@ function addPriorityToSelect(select_id) {
     }
 }
 
+function sortPriorities(priorities) {
+    return priorities.sort(function(a, b) {
+       return a.priority - b.priority;
+    });
+}
+
+function addPriority(priorityList, priority) {
+    if (priorityList.length === 0) {
+        priorityList.push(priority);
+        return priorityList;
+    } else {
+        var sortedList = sortPriorities(priorityList);
+    }
+}
+
+function listItemForPriority(priority) {
+    return "<li class='child'>" + priority.text + "</li>";
+}
+
+function updatePriorityList() {
+    $('#priority-list').empty();
+    chrome.storage.sync.get('priorities', function(result) {
+        var list = result.priorities || {};
+        if (list.toString() === {}.toString()) { return }
+        list.each((function(item) {
+            $('#priority-list').append(listItemForPriority(item));
+        }));
+    });
+}
+
 $(document).ready(function() {
     addTeamsToSelect('#add-team-select');
     addPriorityToSelect('#add-team-priority');
+    updatePriorityList();
 
     var saveTeam = function() {
         chrome.storage.sync.get('priorities', function(list) {
             var priorityList = list;
             if (priorityList.toString() === {}.toString()) { priorityList = [] }
-            priorityList.push({
-            
-            });
+            var updatedList = addPriority(priorityList, /* ADD PRIORITY HERE*/);
+
+            chrome.storage.sync.set({'priorities': updatedList }, updatePriorityList);
         });
     };
 
