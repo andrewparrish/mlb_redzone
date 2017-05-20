fa8a0251fe94398f4a15f1ac59c9d70be39d3aa1
Andrew Parrish <m.andrewparrish@gmail.com>
2017-05-20 15:48:51 -0400

    Setting up some basic testing

---
+++ test/priority_list_test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+
+describe('priority list', function() {
+    it('should properly add an element', function(done) {
+       assert(true);
+       done();
+    });
+});
