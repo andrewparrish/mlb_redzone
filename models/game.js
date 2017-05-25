1f41ac8cb2f23baea51eaa020e2fe1e17ed5c174
Andrew Parrish <m.andrewparrish@gmail.com>
2017-05-24 23:47:38 -0400

    Working on game model

--- models/game.js
+++ models/game.js
@@ -1,6 +1,7 @@
 /**
  * Created by andrewparrish on 5/24/17.
  */
+const GAMES_KEY = 'games';
 
 function Game(teamOne, teamTwo, status) {
     this.teamOne = teamOne;
@@ -8,15 +9,45 @@ function Game(teamOne, teamTwo, status) {
     this.status = status;
     this.lastUpdated = Date.now();
     this.id = null;
-    this.setId = function() {
-        this.id = "" + this.teamOne + this.teamTwo + this.lastUpdated;
+    this._setId = function() {
+        this.id =  this.id || ("" + this.teamOne + this.teamTwo + this.lastUpdated);
     };
-    this.setId();
+    this._setId();
 
     const COMMERCIAL_STATUSES = ["Middle"];
 
+    this._gameAsHash = function() {
+        return {
+            teamOne: this.teamOne,
+            teamTwo: this.teamTwo,
+            status: this.status,
+            lastUpdated: this.lastUpdated,
+            id: this.id
+        }
+    };
+
     this.isInCommercialBreak = function() {
         return COMMERCIAL_STATUSES.indexOf(this.status) !== -1;
-    }
+    };
 
+    this._addGameToArray = function(gamesArray) {
+        gamesArray = gamesArray || [];
+        var gamesIds = gamesArray.map(function(gameHash) {
+            return new Game(gameHash).id;
+        });
+
+        if (gamesIds.indexOf(this.id) !== -1) {
+            gamesArray[gamesIds.indexOf(this.id)] = this;
+        } else {
+            gamesArray = [this._gameAsHash()];
+        }
+
+        return gamesArray;
+    };
+
+    this.saveGame = function() {
+        return chrome.storage.sync.get(GAMES_KEY, function(result) {
+
+        });
+    }.bind(this);
 }
--- test/game_test.js
+++ test/game_test.js
@@ -4,19 +4,58 @@
 
 var expect = require('chai').expect;
 var exec_file = require("../exec_file.js");
-var context = exec_file("models/game.js", { console: console });
-var Game = context.Game;
+var Game = null;
+var chrome = require("sinon-chrome");
+var teamAsHash = null;
 
 describe('game', function() {
-    it('should set its id', function(done) {
-        var game = new Game();
-        expect(game.id).to.not.equal(null);
+    before(function(done) {
+        teamAsHash = { teamOne: 'teamOne', teamTwo: 'teamTwo', status: 'Middle'};
+        global.chrome = chrome;
+
+        chrome.storage.sync.set.yields([teamAsHash]);
+        chrome.storage.sync.get.yields({});
+
+        var context = exec_file("models/game.js", { console: console, chrome: chrome });
+        Game = context.Game;
         done();
     });
 
-    it('should identify its in commercial', function(done) {
-        expect(new Game(null, null, 'Top').isInCommercialBreak()).to.equal(false);
-        expect(new Game(null, null, 'Middle').isInCommercialBreak()).to.equal(true);
-        done();
+    describe('instance methods', function() {
+        it('should set its id', function(done) {
+            var game = new Game();
+            expect(game.id).to.not.equal(null);
+            done();
+        });
+
+        it('should identify its in commercial', function(done) {
+            expect(new Game(null, null, 'Top').isInCommercialBreak()).to.equal(false);
+            expect(new Game(null, null, 'Middle').isInCommercialBreak()).to.equal(true);
+            done();
+        });
+
+        it('should save to chrome storage', function(done) {
+            var games = [new Game(teamAsHash)];
+            expect(new Game(teamAsHash).saveGame()).to.deep.equal(games);
+            done();
+        });
+
+        describe('add Games to Array', function() {
+            before(function(done) {
+                var time = new Date();
+                teamAsHash.lastUpdated = time;
+                done();
+            });
+
+            it('can add games to an empty game array', function(done) {
+                expect(new Game(teamAsHash)._addGameToArray(null)).to.deep.equal([teamAsHash]);
+                expect(new Game(teamAsHash)._addGameToArray([])).to.deep.equal([teamAsHash]);
+                done();
+            });
+
+            it('can add games to a non-empty game array', function(done) {
+                done();
+            });
+        });
     });
 });
