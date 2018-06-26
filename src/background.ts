9569bb7a41cc3056185d5fabb7574555790d4b02
Andrew Parrish <aparrish@jibe.com>
2018-06-26 17:41:01 -0400

    Preloading team data

--- src/background.ts
+++ src/background.ts
@@ -2,6 +2,15 @@ import * as $ from 'jquery';
 import { Game } from './models/game';
 import { GameCheckerService } from './services/game-checker.service';
 
+import { Team } from './models/team';
+
+$.getJSON("mappings/teams.json", (teams) => {
+    $.each(teams, (val, name) => {
+        const team = new Team({ id: val, displayName: name });
+        team.save(Team.uniqueId(val));
+    });  
+});
+
 console.log('HEREEREERE');
 
 const gameChecker = new GameCheckerService();
