1de15e318d5170136e35601019bfc4110298ca8e
Andrew Parrish <aparrish@jibe.com>
2018-06-26 21:59:42 -0400

    Corrected new york team abbreviations

--- src/background.ts
+++ src/background.ts
@@ -6,7 +6,9 @@ import { Team } from './models/team';
 
 $.getJSON("mappings/teams.json", (teams) => {
     $.each(teams, (val, name) => {
-        const team = new Team({ id: val, displayName: name });
+        let blackout = false;
+        if (val === "nym" || val === "nyy" ) { blackout = true; }
+        const team = new Team({ id: val, displayName: name, blackout });
         team.save(Team.uniqueId(val));
     });  
 });
